fc73df7bfba9c008e6fc7c2cefbba4fdd4641fcf
William Lepinski <william.lepinski@rga.com>
2013-11-05 13:15:52 -0200

    Adds other minor tweaks during the choose between stable versions of angular.
    - If the user choose the unstable version, we need to include the ngRoute module
    as a dependency on the main application module. We also need to include the
    angular-route script on the index.html, otherwise the inclusion of the module will
    dispatch an error.

--- app/templates/app/scripts/app.js
+++ app/templates/app/scripts/app.js
@@ -1,13 +1,18 @@
 'use strict';
+(function(){
+<% if (!angularStable) { %>
+    var app = angular.module('<%= _.camelize(appname)%>App', ['ngRoute']);<% } %>
+<% if (angularStable) { %>
+    var app = angular.module('<%= _.camelize(appname)%>App', []);<% } %>
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
 
-angular.module('<%= _.camelize(appname)%>App', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+}.call(this));
--- app/templates/routes/index.js
+++ app/templates/routes/index.js
@@ -1,7 +1,5 @@
 module.exports = function (app) {
     app.get('/', function (req, res, next) {
-        res.render('index', {
-            title: 'Express'
-        });
+        res.render('index');
     });
 };
